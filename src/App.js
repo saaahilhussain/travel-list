f2740b65625f7de1bb3f2226ff446641a4e641af
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-06 09:20:57 +0530

    smoll fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 🌴</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What items would you like to add? : </h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X in your list, you already added X items (X%) </em>
+    </footer>
+  );
+}
