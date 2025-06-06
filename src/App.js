961ba8ddd2a87c433ad9e8ce3efa705c39f73eea
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-06 10:58:02 +0530

    add: form behaviours & hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,21 +22,37 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What items would you like to add? : </h3>
 
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
           <option value={i} key={i}>
             {i}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="items..." />
+      <input
+        type="text"
+        placeholder="items..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>ADD</button>
     </form>
   );
