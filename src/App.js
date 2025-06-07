59d489cc96905667d20733582bf782d83c3d33dc
saaahilhussain <140058338+saaahilhussain@users.noreply.github.com>
2025-06-07 18:14:36 +0530

    stats updated

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -109,10 +109,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list 🚀</p>
+    );
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
+  console.log(packedItems);
   return (
     <footer className="stats">
-      <em>You have X in your list, you already added X items (X%) </em>
+      <em>
+        {percentage === 100
+          ? "You have everything packed! Good to go ✈️"
+          : `You have ${numItems} items in your list, you already added ${packedItems}
+        items (${percentage}%)`}
+      </em>
     </footer>
   );
 }
